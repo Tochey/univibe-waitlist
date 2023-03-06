dd8a6a6de60bd4a11682c8b58e3eb5a70a34a7f9
DON <don@DONs-Laptop-9.local>
2023-03-06 13:21:47 -0500

    update

--- src/pages/api/getAllUsers.ts
+++ src/pages/api/getAllUsers.ts
@@ -14,6 +14,9 @@ export default async function getAll(
       'mongodb+srv://admin:admin@uv-waitlist.nzdhukp.mongodb.net/uv-waitlist?retryWrites=true&w=majority'
     );
     const userCount = await getUserModel().find({}).count();
+    res.setHeader('Access-Control-Allow-Origin', 'https://joinunivibe.com');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     return res.send({ count: userCount });
   } catch (err: any) {
     return res.send({ error: err.message });
--- src/pages/api/join.ts
+++ src/pages/api/join.ts
@@ -12,6 +12,9 @@ export default async function addToWaitList(
       'mongodb+srv://admin:admin@uv-waitlist.nzdhukp.mongodb.net/uv-waitlist?retryWrites=true&w=majority'
     );
     const user = await getUserModel().create({ email: req.body.email });
+    res.setHeader('Access-Control-Allow-Origin', 'https://joinunivibe.com');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     return res.send({ user });
   } catch (err: any) {
     return res.send({ error: err.message });
